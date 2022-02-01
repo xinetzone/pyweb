a5c52486de83aed3029b5370dadc42516f54f04a
xinetzone <xinzone@outlook.com>
2022-02-02 03:55:54 +0800

    new file:   sanicexample.service

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from sanic import Sanic
 from sanic.response import text
 
-app = Sanic("MyHelloWorldApp")
-
+app = Sanic("proxied_example")
+app.config.FORWARDED_SECRET = "YOUR SECRET"
 
 @app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+def index(request):
+    # 此处将会显示公网IP
+    return text(
+        f"{request.remote_addr} connected to {request.url_for('index')}\n"
+        f"Forwarded: {request.forwarded}\n"
+    )
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, workers=8, access_log=False)
